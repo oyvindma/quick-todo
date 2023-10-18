85d9bdbd86e8d5767f8335ee505bf0c9c4a9837c
Øyvind M. Asbjørnsen <oyvindma@gmail.com>
2023-10-18 22:35:08 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+// service-worker.js
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('my-cache').then(function(cache) {
+      return cache.addAll([
+        './',
+        './index.html',
+        './icon.png',
+        // Add more files to cache here
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
